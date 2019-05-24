8b9afa977d3d8dee5a5deaa47e1551ba7351c5bc
rafaelrolivares <rafaelrolivares@gmail.com>
2019-05-24 23:49:15 +0200

    part of part 1

---
+++ src/messages-api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => res.redirect('/messages'))
+
+app.use(bodyParser.json())
+app.post('/messages', (req, res) => {
+    console.log(req.body.text)
+    res.json(req.body.text)
+//    if (!req.body.text) {
+//      res.status(400).json(err)
+//    }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
